a662541a837bd019bc5b6a09321aad1264835b13
andrea.asunsolo <aasunsolor@gmail.com>
2020-09-23 08:37:50 +0800

    data with school mean

--- Script_1_data_wrangling.R
+++ Script_1_data_wrangling.R
@@ -179,49 +179,6 @@ site
 dat$site <- site
 glimpse(dat)
 
-#### trying to get min, max and mean for schools ####
-
-schools<- dat[dat$school_individual == 'School',] %>%
-dplyr:: mutate (school_id = paste0(`Treatment`, opcode)) 
-
-names(schools)
+write.csv(dat, "data_wide_BG_AA.csv")
 
 
-school.mean.var<- dplyr::select(schools, dfs.fid, dfs.prior.1, dfs.prior.2, dfs.prior.3, fid, length, speed.fid, speed.prior.1, speed.prior.2, speed.prior.3, DFSAvg, speed.priorAvg, school_id)
-
-school.mean <- aggregate(school.mean.var[c(1:12)], list (school.mean.var$school_id), MARGIN =  2, FUN = mean)
-
-school.mean$Treatment<- tapply(schools$Treatment, schools$school_id, function(x) x[1])
-
-school.mean$opcode<- tapply(schools$opcode, schools$school_id, function(x) x[1])
-
-school.mean$family<- tapply(schools$family, schools$school_id, function(x) x[1])
-
-school.mean$genus<- tapply(schools$genus, schools$school_id, function(x) x[1])
-
-school.mean$species<- tapply(schools$species, schools$school_id, function(x) x[1])
-
-school.mean$activity<- tapply(schools$activity, schools$school_id, function(x) x[1])
-
-school.mean$school_individual<- tapply(schools$school_individual, schools$school_id, function(x) x[1])
-
-school.mean$scientific<- tapply(schools$scientific, schools$school_id, function(x) x[1])
-
-school.mean$site<- tapply(schools$site, schools$school_id, function(x) x[1])
-
-school.mean<- dplyr::rename(school.mean, unique_id = Group.1 )
-dat1<-dplyr::filter(dat, !school_individual=="School")
-dat<- rbind(dat1,school.mean)
-#### max ####
-school.max<- aggregate(school.mean.var[c(1:12)], list (school.mean.var$school_id), MARGIN =  2, FUN = max)
-
-school.max
-
-### Min #####
-school.min<- aggregate(school.mean.var[c(1:12)], list (school.mean.var$school_id), MARGIN =  2, FUN = min)
-
-school.min
-
-
-
-write.csv(dat, "data_wide_SchoolMean_BG_AA.csv")
